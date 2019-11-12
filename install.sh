b35e9a9124752832618a99901b4bf11c90b859c3
chandrasekar <chandrasekar.gopal@cubforce.com>
2019-11-12 18:11:13 +0530

    Adding docker file

---
+++ install.sh
@@ -0,0 +1,29 @@
+# General alpine linux updated
+apk update
+
+# Install virtual package list 
+apk add --no-cache --virtual .build-deps
+
+# Adding curl and gnupg  
+apk add curl bash gnupg
+
+# Adding nodejs
+apk add nodejs 
+
+# install yarn
+apk add yarn
+
+# Install aws cli dependency python 
+apk add python py-pip openssl ca-certificates
+
+# install aws cli 
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+#rm cach
+apk --purge -v del py-pip
+rm /var/cache/apk/*
+
+
+
